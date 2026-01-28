5588023ca4cf7b48e15df6132cbd26ade70c477d
𝒩𝑜𝓏𝒾𝓁𝒶 <nozilarajabboyeva@gmail.com>
2026-01-28 16:43:32 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+#16-misol
+matn = ""
+print("".isspace())
+
+#17-misol
+matn = "SALOM"
+print("SALOM".isupper())
+
+#18-misol
+matn = "salom"
+print(matn.islower())
+
+#19-misol
+matn = "Pyhton"
+print(matn.center())
+
+#20-misol
+matn = "Python"
+print(matn.center())
+
+#21-misol
+matn = "Python"
+print(matn.center())
+
+#22-misol
+matn = "42"
+print(matn.zfill(6))
+
+#23-misol
+matn = "banana"
+print("banana".replace("a", "@"))
